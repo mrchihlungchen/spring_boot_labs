6ea463be7467a1d5cddb2dc92c12f786c5135ee9
tony_ucom <tony@uuu.com>
2022-04-16 11:59:13 +0800

    init demo1

---
+++ demo1/src/test/kotlin/com/taishinbank/demo1/Demo1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.taishinbank.demo1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
