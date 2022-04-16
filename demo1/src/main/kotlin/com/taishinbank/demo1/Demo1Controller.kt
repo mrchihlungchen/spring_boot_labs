45a4aee05aeb35990399fb5a9da260b8ac97cfda
tony_ucom <tony@uuu.com>
2022-04-16 16:34:29 +0800

    pass parameter from controller to html

--- demo1/src/main/kotlin/com/taishinbank/demo1/Demo1Controller.kt
+++ demo1/src/main/kotlin/com/taishinbank/demo1/Demo1Controller.kt
@@ -1,10 +1,21 @@
 package com.taishinbank.demo1
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class Demo1Controller {
     @GetMapping("/")
-    fun url(): String = "demo1"
+    fun url(model: Model): String {
+
+        model.addAttribute(
+                "message_from_controller",
+                "This message is from controller."
+        )
+
+        return "demo1"
+    }
+
+
 }
